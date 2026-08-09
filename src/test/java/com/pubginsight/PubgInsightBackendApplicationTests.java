15d972e69831391716532e3a29cb6b7516b2a88b
Đông Nghi (Lucie) <S3914633@rmit.edu.vn>
2026-08-09 23:37:38 +0700

    feat: connect frontend and backend with health check

---
+++ src/test/java/com/pubginsight/PubgInsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pubginsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubgInsightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
